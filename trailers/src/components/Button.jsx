9fee5ed0a474e400e794e78bc06abebbbf31a1d7
rthrx44 <bkbrx44@gmail.com>
2025-08-12 19:25:00 +0800

    add hover state on button

--- trailers/src/components/Button.jsx
+++ trailers/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ icon = "", displayText = "WATCH NOW" }) => {
   return (
-    <button className="flex items-center justify-center h-fit bg-button rounded-full gap-1 md:gap-1.5 lg:gap-2 px-3 py-1.5 md:px-4 md:py-2 lg:px-5 lg:py-2.5">
+    <button className="flex items-center justify-center h-fit bg-button rounded-full gap-1 md:gap-1.5 lg:gap-2 px-3 py-1.5 md:px-4 md:py-2 lg:px-5 lg:py-2.5 hover:bg-bg-button">
       <div className="flex items-center text-main size-3 md:size-4 lg:size-[1.125rem]">
         {icon}
       </div>
